c07bfc7e25183f81f0feaf28101564d9f217aa1c
fgouvea10 <felipegalvesouvea@gmail.com>
2024-03-14 18:23:36 -0300

    fix: remove object curly spacing

--- node.js
+++ node.js
@@ -37,7 +37,6 @@ module.exports = {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "error",
     "no-console": ["error", { "allow": ["warn", "error"] }],
-    "object-curly-spacing": ["error", "never"],
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "error"
   },
--- react.js
+++ react.js
@@ -49,7 +49,6 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "error",
     "no-console": ["error", { "allow": ["warn", "error"] }],
     "react/jsx-curly-brace-presence": ["error", { "props": "never" }],
-    "object-curly-spacing": ["error", "never"],
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "error"
   },
